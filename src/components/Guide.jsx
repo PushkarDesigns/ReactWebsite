8d9d991bb6a30ca0b7af747d1f96e561e9a36ed9
Pushkar <rawatprince815@gmail.com>
2025-11-07 19:43:27 +0530

    ready!

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -1,13 +1,14 @@
-const Guide = (props) => 
-{
-    return(
+const Guide = (props) => {
+    return (
+        <>
         <div className="area">
             <div className="container">
-                    <img src={props.image.src} alt={props.image.src}/>
-                    <h3 className="heading">{props.heading}</h3>
-                    <p className="paragraph">{props.paragraph}</p>
+                <img src={props.image.src} alt={props.image.src} />
+                <h3 className="heading">{props.heading}</h3>
+                <p className="paragraph">{props.paragraph}</p>
             </div>
         </div>
+        </>
     )
 }
 
